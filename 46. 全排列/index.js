09a4d2a8de1f3e5320f7748905f1f11f0a884fd4
杨帝林 <1004137895@qq.com>
2020-04-26 16:02:30 +0800

    46

--- 46. 全排列/index.js
+++ 46. 全排列/index.js
@@ -1,17 +1,55 @@
 // 求出个数
-var permute = function(nums) {
+var permute = function (nums) {
     const len = nums.length;
-    const fn = (n)=>{
-        if(n === 1) return n
-        return fn(n-1)*n
-    }
+    const fn = (n) => {
+        if (n === 1) return n;
+        return fn(n - 1) * n
+    };
     return fn(len)
-}
+};
 // 求出排列
-var permute = function(nums) {
-    const res = []
-    for(let i = 0; i<nums.length;i++){
-        
-    }
-}
-console.log(permute([1,2,3]));
+// 思路 ： 固定一个数字，剩余的数字也是按照这个规律组合，接着指针往前走，即f(n) = nf(n-1)
+// 由于每一次查找都是查找到nums.length 才往上走，可以使用栈的特性记录,一旦达到终点就退出
+// 这是栈与递归的有机结合
+var permute = function (nums) {
+    const res = [];
+    const stacks = [];
+    const len = nums.length;
+    let curValue;
+    const backtrack = () => {
+        if (stacks.length === len) {
+            res.push(stacks.slice());
+            return
+        }
+        for (let i = 0; i < len; i++) {
+            curValue = nums[i];
+            if (!stacks.includes(curValue)) {
+                stacks.push(curValue);
+                backtrack();
+                stacks.pop()
+            }
+        }
+    };
+    backtrack();
+    return res
+};
+// 官方思路：
+// 优点：
+// 1. 没使用栈去缓存数据
+// 2. 成功去掉了 需要判断栈里元素是否有重复的逻辑，避免无谓遍历
+// permute = function (nums) {
+//     const len = nums.length;
+//     const res = [];
+//     const backtrack = (curIndex) => {
+//         if (curIndex === len) return res.push(nums.slice());
+//         for (let i = curIndex; i < len; i++) {
+//             [nums[i], nums[curIndex]] = [nums[curIndex], nums[i]];
+//             backtrack(curIndex + 1);
+//             [nums[i], nums[curIndex]] = [nums[curIndex], nums[i]]
+//         }
+//     };
+//     backtrack(0);
+//     return res
+// };
+
+console.log(permute([1, 2, 3]));
--- 面试题51. 数组中的逆序对/index.js
+++ 面试题51. 数组中的逆序对/index.js
@@ -1,12 +1,76 @@
-var reversePairs = function(nums) {
-    let num = 0
-    for(let i =0,len = nums.length;i<len-1;i++){
-        const cur = nums[i]
-        for(let j = i+1 ; j<len;j++){
-            const next = nums[j]
+// 1. 原始解法
+// 通过双层循环
+var reversePairs = function (nums) {
+    let num = 0;
+    for (let i = 0, len = nums.length; i < len - 1; i++) {
+        const cur = nums[i];
+        for (let j = i + 1; j < len; j++) {
+            const next = nums[j];
             cur > next && num++
         }
     }
     return num
 };
+// 利用归并排序实现，为什么想到归并排序？因为归并排序在排序过程中会进行左右比较，
+// 并且比较之后就会合并，不会依次比较
+// 1. 实现归并排序
+
+const arr = [5, 7, 6, 2, 3];
+const mergeSort = (arr) => {
+    // 1.分
+    if (arr.length < 2) return arr;
+    const aim = Math.floor(arr.length / 2);
+    const leftArr = arr.splice(0, aim);
+    const rightArr = arr;
+    const curLeftArr = mergeSort(leftArr);
+    const curRightArr = mergeSort(rightArr);
+    // 2. 治
+    const res = [];
+    // 2.1 比较
+    while (curLeftArr.length) {
+        if (!curRightArr.length) {
+            break
+        }
+        if (curLeftArr[0] <= curRightArr[0]) {
+            res.push(curLeftArr.shift())
+        } else {
+            res.push(curRightArr.shift())
+        }
+    }
+    // 2.2 处理剩余
+    if (curRightArr.length) res.push(...curRightArr);
+    if (curLeftArr.length) res.push(...curLeftArr);
+    return res
+};
+reversePairs = function (nums) {
+    let num = 0;
+    const fn = (arr) => {
+        if (arr.length < 2) return arr;
+        // 分
+        const aim = Math.floor(arr.length / 2);
+        const leftArr = arr.slice(0, aim);
+        const rightArr = arr.slice(aim);
+        const resLeftArr = fn(leftArr);
+        const resRightArr = fn(rightArr);
+        // 治
+        const res = [];
+        while (resLeftArr.length && resRightArr.length) {
+            if (resLeftArr[0] > resRightArr[0]) {
+                num +=  resRightArr.length;
+                res.push(resLeftArr.shift())
+            } else {
+                res.push(resRightArr.shift())
+            }
+        }
+        if (resLeftArr.length) res.push(...resLeftArr);
+        if (resRightArr.length) {
+            res.push(...resRightArr)
+        }
+        return res
+    };
+   fn(nums);
+    return num
+};
+// console.log(mergeSort([1, 7, 2, 3, 6, 8]));
+
 console.log(reversePairs([7,5,6,4]));
