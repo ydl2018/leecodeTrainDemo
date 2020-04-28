633ec4042900690d4273fed96ddedea21c4bc03b
杨帝林 <1004137895@qq.com>
2020-04-28 16:23:23 +0800

    23

--- 23. 合并K个排序链表/index.js
+++ 23. 合并K个排序链表/index.js
@@ -17,7 +17,83 @@
     // 将node1添加到当前resNode的next属性
     // 如果当前Map数组都是空节点，那么结束遍历
 // 返回resNode
-var mergeKLists = function(lists) {
-    let resNode = null;
-    // while ()
-};
+var mergeKLists = function (lists) {
+        let resNode, originNode;
+        resNode = originNode = {val: null, next: null};
+        const Map = lists.filter(v => v && v.val !== null)
+        while (Map.length) {
+            const min = Math.min(...Map.map(v => v.val))
+            // 获取最小的node节点
+            const curIndex = Map.findIndex(v => v.val === min)
+            const curNode = Map[curIndex];
+            resNode.next = curNode;
+            resNode = resNode.next;
+            if (curNode.next !== null) {
+                Map[curIndex] = curNode.next;
+            } else {
+                Map.splice(curIndex, 1)
+            }
+        }
+        return originNode.next
+    };
+// 分治 分为两个链表，依次合并
+var mergeKLists = function (lists) {
+    const fn = (lists, start, end) => {
+        if (start === end) return lists[start]
+        const mid = Math.floor((start + end) / 2);
+        let l1 = fn(lists, start, mid);
+        let l2 = fn(lists, mid+1, end)
+        if (!l1 || !l2) return l1 ? l1 : l2;
+
+        let resPtr;
+        const headPtr =  resPtr = {val:null,next:null};
+        while (l1 && l2) {
+            if (l1.val > l2.val) {
+                resPtr.next = l2;
+                resPtr = resPtr.next;
+                l2 = l2.next;
+            } else {
+                resPtr.next = l1;
+                resPtr = resPtr.next;
+                l1 = l1.next;
+            }
+        }
+        if(l1){
+            resPtr.next = l1
+        }else{
+            resPtr.next = l2
+        }
+        return headPtr.next
+    }
+    if(!lists.length) return lists
+    return fn(lists,0,lists.length-1)
+}
+// 从c++ 解法推出的分治 todo
+var mergeKLists = function (lists) {
+    const merge = (l1,l2)=>{
+        let headPtr, originHeadPtr;
+        headPtr = originHeadPtr = {val:null,next:null}
+        while (l1 && l2){
+            if(l1.val > l2.val){
+                headPtr.next = l1
+                l1 = l1.next;
+            }else{
+                headPtr.next = l2;
+                l2 = l2.next
+            }
+        }
+        if(l1){
+            headPtr.next = l1
+        }else{
+            headPtr.next = l2
+        }
+        return originHeadPtr.next
+    }
+    // for(let i =0 ; i)
+}
+const modelData = [
+    {val: 1, next: {val: 4, next: {val: 5, next: null}}},
+    {val: 1, next: {val: 3, next: {val: 4, next: null}}},
+    {val: 2, next: {val: 6, next: null}}
+]
+console.log(mergeKLists(modelData));
